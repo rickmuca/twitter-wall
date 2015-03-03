5bd94fb90709035b682becfbfc8833b32997c2dc
Rickmuca <ricardomc.universidad@gmail.com>
2015-03-03 16:33:17 +0100

    Wall working

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.resolve(__dirname + '/../views/index.html'));
+/* GET moderator page. */
+router.get('/moderator', function(req, res, next) {
+  res.sendFile(path.resolve(__dirname + '/../views/moderator.html'));
 });
+
+/* GET wall page. */
+router.get('/wall', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname + '/../views/wall.html'));
+});
+
+
 
 module.exports = router;
